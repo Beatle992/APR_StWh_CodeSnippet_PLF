ad2e3cc897235d35baabcee5bae9ffbf0484dd59
leo.pirringer <leo.pirringer@sz-ybbs.ac.at>
2023-01-21 15:33:48 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Wheater wheater = new Wheater("São Paulo", 28);
+        System.out.println(wheater.getCity());
+        System.out.println(wheater.calculateCelsius());
+        System.out.println(wheater.calculateFahrenheit());
+        System.out.println(wheater.calculateKelvin());
+
     }
 }
